e3035a2bd9ddab60fb1f319136cc04112b6d9cb5
Yoichi Imai <sunnyone41@gmail.com>
2022-08-14 22:58:53 +0900

    Use gio for resources

--- src/static_resource.rs
+++ src/static_resource.rs
@@ -1,16 +1,9 @@
-extern crate gio_sys;
-extern crate glib_sys;
-extern crate libc;
-
-use std;
+extern crate gio;
 
 pub fn init() {
     let res_bytes = include_bytes!("resources/resources.gresource");
-    unsafe {
-        // gbytes and resource will not be freed
-        let gbytes = glib_sys::g_bytes_new(res_bytes.as_ptr() as *const libc::c_void,
-                                           res_bytes.len());
-        let resource = gio_sys::g_resource_new_from_data(gbytes, std::ptr::null_mut());
-        gio_sys::g_resources_register(resource);
-    }
+    // gbytes and resource will not be freed
+    let gbytes = glib::Bytes::from(res_bytes);
+    let resource = gio::Resource::from_data(&gbytes).unwrap();
+    gio::functions::resources_register(resource)
 }
