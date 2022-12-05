ddcff197ead1e8e7887b89bd2d3e03973ce00322
Yoichi Imai <sunnyone41@gmail.com>
2022-12-05 21:19:03 +0900

    Use modern interface of cairo

--- src/station_renderer.rs
+++ src/station_renderer.rs
@@ -1,6 +1,6 @@
 use std::f64::consts;
 use railway;
-use cairo::LineCap;
+use gtk::cairo::LineCap;
 
 fn commit_dot_box_size(cell_height: i32) -> (i32, i32) {
     (cell_height, cell_height)
@@ -24,12 +24,12 @@ fn box_x(start_x: i32, box_width: i32, index: usize) -> i32 {
 }
 
 pub fn render(station: &railway::RailwayStation,
-              context: &cairo::Context,
+              context: &gtk::cairo::Context,
               bg_area: &gtk::Rectangle,
               cell_area: &gtk::Rectangle)
               -> (gtk::Rectangle, gtk::Rectangle) {
 
-    let (box_width, box_height) = commit_dot_box_size(cell_area.height);
+    let (box_width, box_height) = commit_dot_box_size(cell_area.height());
     let dot_radius = commit_dot_radius(box_width, box_height);
 
     // let c = 0.1 * ((private.num + 1) as f64);
@@ -38,8 +38,8 @@ pub fn render(station: &railway::RailwayStation,
     context.set_line_cap(LineCap::Square);
 
     for track in &station.tracks {
-        let track_box_x = box_x(cell_area.x, box_width, track.track_number.as_usize()) as f64;
-        let track_box_y = cell_area.y as f64;
+        let track_box_x = box_x(cell_area.x(), box_width, track.track_number.as_usize()) as f64;
+        let track_box_y = cell_area.y() as f64;
 
         let center_x = track_box_x + box_width as f64 / 2.0;
         let center_y = track_box_y + box_height as f64 / 2.0;
@@ -47,14 +47,14 @@ pub fn render(station: &railway::RailwayStation,
         let merge_line_offset = merge_line_offset(box_height) as f64;
 
         if !track.from_tracks.is_empty() {
-            let top_y = bg_area.y;
+            let top_y = bg_area.y();
 
             context.move_to(center_x, top_y as f64 + merge_line_offset);
             context.line_to(center_x, center_y as f64);
             context.stroke();
 
             for num in &track.from_tracks {
-                context.move_to((box_x(cell_area.x, box_width, num.as_usize()) +
+                context.move_to((box_x(cell_area.x(), box_width, num.as_usize()) +
                                  box_width / 2) as f64 + 1.0,
                                 top_y as f64);
                 context.line_to(center_x, top_y as f64 + merge_line_offset);
@@ -63,7 +63,7 @@ pub fn render(station: &railway::RailwayStation,
         }
 
         if !track.to_tracks.borrow().is_empty() {
-            let bottom_y = bg_area.y + bg_area.height;
+            let bottom_y = bg_area.y() + bg_area.height();
 
             context.move_to(center_x, center_y as f64);
             context.line_to(center_x, bottom_y as f64 - merge_line_offset);
@@ -71,7 +71,7 @@ pub fn render(station: &railway::RailwayStation,
 
             for num in track.to_tracks.borrow().iter() {
                 context.move_to(center_x, bottom_y as f64 - merge_line_offset);
-                context.line_to((box_x(cell_area.x, box_width, num.as_usize()) +
+                context.line_to((box_x(cell_area.x(), box_width, num.as_usize()) +
                                  box_width / 2) as f64 + 1.0,
                                 bottom_y as f64);
                 context.stroke();
@@ -91,6 +91,6 @@ pub fn render(station: &railway::RailwayStation,
     }
 
     let tracks_width = calc_tracks_width(station, box_width);
-    (gtk::Rectangle { x: bg_area.x + tracks_width, ..*bg_area },
-     gtk::Rectangle { x: cell_area.x + tracks_width, ..*cell_area })
+    (gtk::Rectangle::new(bg_area.x() + tracks_width, bg_area.y(), bg_area.width(), bg_area.height()),
+     gtk::Rectangle::new(cell_area.x() + tracks_width, cell_area.y(), cell_area.width(), cell_area.height()))
 }
